f93762cfa63839105f01f996e4ba80c50d377cc3
Anan_749 <yicmeng@outlook.com>
2021-08-25 09:32:41 +0800

    <fix>:<edit pay/prepay>

--- application/controllers/WxPayControl.php
+++ application/controllers/WxPayControl.php
@@ -22,23 +22,29 @@ class WxPayControl extends CI_Controller{
         $result = get_prepay_id($id,$price,$openid,$description);
         $msg = $result['return_msg'];
         $res = null;
-        if($result['result_code'] === 'SUCCESS'){
-            $appId = $result['appid'];
-            $nonceStr = $result['nonce_str'];
-            $prepay_id = $result['prepay_id'];
-            $timeStamp = time();
-            $key = 'Hftx780125780125780125780125Hftx';
-            $paySign = md5("appId=$appId&nonceStr=$nonceStr&package=prepay_id=$prepay_id&signType=MD5&timeStamp=$timeStamp&key=$key");
-            $state = get_prepay_state($id);
-            $res = [
-                'nonceStr' => $nonceStr,
-                'prepay_id' => $prepay_id,
-                'timeStamp' => strval($timeStamp),
-                'paySign' => $paySign,
-                'signType' => 'MD5'
-            ];
-            $resultArr = build_resultArr('GPI000', TRUE, 0, $msg, [$res,$result,$state,$id]);
-            http_data(200, $resultArr, $this);
+        if(isset($result['return_code'])){
+            $msg = $result['return_msg'];
+        }
+        if(isset($result['result_code'])){
+            $msg = $result['result_code'];
+            if($result['result_code'] === 'SUCCESS'){
+                $appId = $result['appid'];
+                $nonceStr = $result['nonce_str'];
+                $prepay_id = $result['prepay_id'];
+                $timeStamp = time();
+                $key = 'Hftx780125780125780125780125Hftx';
+                $paySign = md5("appId=$appId&nonceStr=$nonceStr&package=prepay_id=$prepay_id&signType=MD5&timeStamp=$timeStamp&key=$key");
+                $state = get_prepay_state($id);
+                $res = [
+                    'nonceStr' => $nonceStr,
+                    'prepay_id' => $prepay_id,
+                    'timeStamp' => strval($timeStamp),
+                    'paySign' => $paySign,
+                    'signType' => 'MD5'
+                ];
+                $resultArr = build_resultArr('GPI000', TRUE, 0, $msg, [$res,$result,$state,$id]);
+                http_data(200, $resultArr, $this);
+            }
         }
         $resultArr = build_resultArr('GPI001', FALSE, 0, $msg, $res);
         http_data(200, $resultArr, $this);
